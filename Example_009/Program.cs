58e62e3629ca08ddd21e4bcb8fa226bf6a83f60f
Sam <kipriyanov.1991@mail.ru>
2022-06-29 15:17:09 +0300

    Поиск элемента в массиве

---
+++ Example_009/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 4, 8, 15, 16, 23, 41, 21, 77, 99, 41 };
+int n = array.Length;
+int find = 41;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
+
+
+
